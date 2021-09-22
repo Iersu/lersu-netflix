5eac35ee9446957e1ec9632e32cf7b062014c75e
lersu <nemanja.pavlovic.235.19@ict.edu.rs>
2021-09-22 17:39:16 +0200

    depoy my react app to github pages

---
+++ src/context/signup-context.js
@@ -0,0 +1,13 @@
+import { createContext, useState} from "react";
+
+const SignUpContext = createContext()
+
+function SignUpContextProvider  ({ children })  {
+    const [email, setEmail] = useState("")
+
+    return  <SignUpContext.Provider value={{ email, setEmail }}>{children}</SignUpContext.Provider>
+
+
+}
+
+export { SignUpContextProvider, SignUpContext }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { GlobalStyles } from './globalstyles';
+import { SignUpContextProvider } from './context/signup-context';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { FirebaseContext } from "./context/firebase"
+import ScrollToTop from './utils/ScrollToTop';
+
+const config = {
+  apiKey: "AIzaSyDNbLr3Qku5OtA6qEgcbIXpjeFvdlH0i1s",
+  authDomain: "netflix-35a46.firebaseapp.com",
+  databaseURL: "https://netflix-35a46-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "netflix-35a46",
+  storageBucket: "netflix-35a46.appspot.com",
+  messagingSenderId: "120632586146",
+  appId: "1:120632586146:web:6e2a709d535ede5e040cfb",
+  measurementId: "G-FWXDS3JM81"
+}
+
+const firebase = window.firebase.initializeApp(config)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <React.StrictMode> 
+    <Router>
+      <ScrollToTop/>
+      <FirebaseContext.Provider value={{ firebase: window.firebase }} >
+        <SignUpContextProvider>
+          <GlobalStyles/>
+          <App />
+        </SignUpContextProvider>
+      </FirebaseContext.Provider>
+    </Router>
+ </React.StrictMode>
+, 
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
